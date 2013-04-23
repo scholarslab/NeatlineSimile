9c4a382d8b0e0fc9fee28f8c144bee0d9828c29b
David McClure <david.mcclure@virginia.edu>
2013-04-23 16:02:09 -0400

    Gets Jasmine tests running with manual document.write() calls to load SIMILE.

---
+++ tests/jasmine/helpers/global.js
@@ -0,0 +1,16 @@
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=76; */
+
+/**
+ * @package     omeka
+ * @subpackage  neatline-Simile
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+
+/**
+ * Load SIMILE.
+ */
+document.write('<script type="text/javascript" src="/components/simile/timeline_ajax/simile-ajax-api.js?bundle=true"></script>');
+document.write('<script type="text/javascript" src="/components/simile/timeline_js/timeline-api.js?bundle=true"></script>');
