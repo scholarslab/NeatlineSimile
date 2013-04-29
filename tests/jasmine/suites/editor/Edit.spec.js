7168e26d828ed70200f47508e9f0a9d2f867c57d
David McClure <david.mcclure@virginia.edu>
2013-04-29 09:57:11 -0400

    Shells out tests for defaults manifestation/previewing.

---
+++ tests/jasmine/suites/editor/Edit.spec.js
@@ -0,0 +1,31 @@
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=76; */
+
+/**
+ * @package     omeka
+ * @subpackage  neatline-Simile
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+describe('Form Edit', function() {
+
+
+  beforeEach(function() {
+    SM.loadEditor();
+  });
+
+
+  afterEach(function() {
+    // TODO: Should re-render events.
+  });
+
+
+  it('should update track height');
+  it('should update tape height');
+  it('should update interval pixels');
+  it('should update interval unit');
+  it('should update default date');
+
+
+});
---
+++ tests/jasmine/suites/public/ExhibitDefaults.spec.js
@@ -0,0 +1,27 @@
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=76; */
+
+/**
+ * @package     omeka
+ * @subpackage  neatline-Simile
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+describe('Exhibit Defaults', function() {
+
+
+  beforeEach(function() {
+    SM.loadNeatline();
+  });
+
+
+  it('should set track height');
+  it('should set tape height');
+  it('should set interval pixels');
+  it('should set interval unit');
+  it('should set default date');
+  it('should set current date');
+
+
+});
